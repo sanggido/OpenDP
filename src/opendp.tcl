a2066a210b80c4e0ff17a41bc3758f8951a2f44d
James Cherry <cherry@parallaxsw.com>
2019-11-23 08:43:46 -0700

    row check

--- src/opendp.tcl
+++ src/opendp.tcl
@@ -5,17 +5,18 @@ proc legalize_placement { args } {
   sta::parse_key_args "legalize_placement" args \
     keys {-constraints} flags {}
 
-  set odp [get_opendp]
-  if { [db_has_rows] } {
-    if { [info exists keys(-constraints)] } {
-      set constraints_file $keys(-constraints)
-      if { [file readable $constraints_file] } {
-	$odp read_constraints $constraint_file
-      }
+  if { [info exists keys(-constraints)] } {
+    set constraints_file $keys(-constraints)
+    if { [file readable $constraints_file] } {
+      $odp read_constraints $constraint_file
+    } else {
+      puts "Warning: cannot read $constraints_file"
     }
-  } else {
-    puts "ERROR: no rows defined in design. Use initialize_floorplan to add rows."
   }
-
-  $odp legalize_place
+  if { [db_has_rows] } {
+    set odp [get_opendp]
+    $odp legalize_place
+  } else {
+    puts "Error: no rows defined in design. Use initialize_floorplan to add rows."
+  }
 }
